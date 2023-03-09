cc4ac05e3678de6b88a178da4d844d9ba11629bc
Andrey <aimm.legate@gmail.com>
2023-03-09 20:37:36 +0400

    basic test

--- contracts/NftPoops.sol
+++ contracts/NftPoops.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftPoops is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
---
+++ test/NftPoops.js
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+
+describe("Nft-Poops", function () {
+  it("Mint and transfer", async () => {
+    const NftPoops = await ethers.getContractFactory("NftPoops");
+    const nftPoops = await NftPoops.deploy();
+
+    await nftPoops.deployed();
+
+    const recipient = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+
+    const metadataURI = "cid/test.png";
+
+    let balance = await nftPoops.balanceOf(recipient);
+    expect(balance).to.equal(0);
+
+    const newMintedToken = await nftPoops.payToMint(recipient, metadataURI, {
+      value: ethers.utils.parseEther("0.05"),
+    });
+  });
+});
